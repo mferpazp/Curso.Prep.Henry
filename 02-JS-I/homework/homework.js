057dc7fbb8d2e60bc4a8227fbb42b626fbfa5004
unknown <mferpazp@gmail.com>
2021-11-22 18:06:59 -0500

    controldeflujo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -48,20 +48,24 @@ function resta(x, y) {
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+return (x*y)  
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return (x/y)
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (x == y) {
+    return true
+  }
+
+
 }
 
 function tienenMismaLongitud(str1, str2) {
